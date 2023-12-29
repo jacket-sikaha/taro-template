ef0508ea86f2422c336104e450e8e7f1c0687f95
xuhaoxian <xuhaoxian@gdlinkjob.com>
2023-12-29 12:02:09 +0800

    mobx测试demo
    检验mobx配合context使用

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,29 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.less'
+import { View, Text, Button } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import "./index.less";
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
+      <button
+        onClick={() => {
+          Taro.navigateTo({ url: "/pages/test/index" });
+        }}
+      >
+        test
+      </button>
+      <Button
+        onClick={() => {
+          Taro.navigateTo({ url: "/pages/user/index" });
+        }}
+      >
+        user
+      </Button>
     </View>
-  )
+  );
 }
